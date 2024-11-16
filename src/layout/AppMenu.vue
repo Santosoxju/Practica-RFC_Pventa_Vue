d2de6c6ae7fd7e574b34e13c328b873e9fe2435e
Santos Abelino Oxlaj Juarez <santosoxju@gmail.com>
2024-11-15 20:18:46 -0600

    Add files via upload

---
+++ src/layout/AppMenu.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue';
+
+import AppMenuItem from './AppMenuItem.vue';
+
+const model = ref([
+    {
+        label: 'Universidad Autónoma de Chiapas',
+        items: [{ label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/' }]
+    },
+    {
+        label: 'UI Components',
+        items: [
+            { label: 'Demo Clientes', icon: 'pi pi-fw pi-user', to: '/formClientes' },
+            { label: 'Punto de venta', icon: 'pi pi-fw pi-cart-plus', to: '/puntoVenta' },
+            { label: 'Consultar API', icon: 'pi pi-fw pi-code', to: '/apiConsumir' }
+        ]
+    }
+]);
+</script>
+
+<template>
+    <ul class="layout-menu">
+        <template v-for="(item, i) in model" :key="item">
+            <app-menu-item v-if="!item.separator" :item="item" :index="i"></app-menu-item>
+            <li v-if="item.separator" class="menu-separator"></li>
+        </template>
+    </ul>
+</template>
+
+<style lang="scss" scoped></style>
